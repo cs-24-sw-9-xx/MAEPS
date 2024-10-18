3abf452a1157fa1f0eb27322a5da25e66262b23b
Henrik van Peet <henrik_peet@hotmail.com>
2024-10-18 10:12:58 +0200

    Add communication

--- Assets/Scripts/ExplorationAlgorithm/HenrikAlgo/HenrikExplorationAlgorithm.cs
+++ Assets/Scripts/ExplorationAlgorithm/HenrikAlgo/HenrikExplorationAlgorithm.cs
@@ -1,6 +1,8 @@
 // Copyright 2024
 // Contributors: Henneboy
 
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Maes.Map;
 using Maes.Robot;
@@ -13,6 +15,7 @@ namespace Maes.ExplorationAlgorithm.HenrikAlgo
     {
         private IRobotController _robotController;
         private Vector2Int? _targetTile = null;
+        private uint _ticksSinceHeartbeat = 0;
         public HenrikExplorationAlgorithm()
         {
         }
@@ -29,6 +32,8 @@ namespace Maes.ExplorationAlgorithm.HenrikAlgo
 
         public void UpdateLogic()
         {
+            ShareSlamMap();
+
             var status = _robotController.GetStatus();
             if (status == RobotStatus.Idle)
             {
@@ -46,6 +51,28 @@ namespace Maes.ExplorationAlgorithm.HenrikAlgo
                 }
                 return;
             }
+        }
+
+        private void ShareSlamMap()
+        {
+            if (_ticksSinceHeartbeat == 10)
+            {
+                Debug.Log("Sent slam");
+                var ownHeartbeat = new HeartbeatMessage(_robotController.GetSlamMap());
+                _ticksSinceHeartbeat = 0;
+                _robotController.Broadcast(ownHeartbeat);
+            }
+            var receivedHeartbeat = new Queue<HeartbeatMessage>(_robotController.ReceiveBroadcast().OfType<HeartbeatMessage>());
+            if (receivedHeartbeat.Count > 1)
+            {
+                Debug.Log("Recieved slam");
+                var combinedMessage = receivedHeartbeat.Dequeue();
+                foreach (var message in receivedHeartbeat)
+                {
+                    combinedMessage = combinedMessage.Combine(message);
+                }
+            }
+            _ticksSinceHeartbeat++;
         }
 
         private void CollisionCorrector()
@@ -66,6 +93,32 @@ namespace Maes.ExplorationAlgorithm.HenrikAlgo
             info.AppendLine($"Status: {_robotController.GetStatus()}.");
 
             return info.ToString();
+        }
+    }
+
+    public class HeartbeatMessage
+    {
+        internal SlamMap map;
+
+        public HeartbeatMessage(SlamMap map)
+        {
+            this.map = map;
+        }
+
+        public HeartbeatMessage Combine(HeartbeatMessage otherMessage)
+        {
+            if (otherMessage is HeartbeatMessage heartbeatMessage)
+            {
+                List<SlamMap> maps = new() { heartbeatMessage.map, map };
+                SlamMap.Synchronize(maps); //layers of pass by reference, map in controller is updated with the info from message
+                return this;
+            }
+            return null;
+        }
+
+        public HeartbeatMessage Process() //Combine all, then process, but not really anything to process for heartbeat
+        {
+            return this;
         }
     }
 }
