ad29478f86691e8a8f7a157427e1c5956d38c25b
S3JER <72788875+S3JER@users.noreply.github.com>
2024-10-29 09:56:42 +0100

    Formated file + Namespace

--- Assets/Scripts/UI/Patrolling/StoppingCriteria.cs
+++ Assets/Scripts/UI/Patrolling/StoppingCriteria.cs
@@ -2,37 +2,40 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class StoppingCriteria : MonoBehaviour
+namespace MAEPS.UI
 {
-    public Scrollbar progressBar;
-    public TextMeshProUGUI cycleText;
-    public TextMeshProUGUI stoppingCriteriaEnbaledText;
-    private int _totalCycles;
-    private int _currentCycles = 0;
-
-    public StoppingCriteria(int cycles, bool idleStop)
+    public class StoppingCriteria : MonoBehaviour
     {
-        _totalCycles = cycles;
-        stoppingCriteriaEnbaledText.text = idleStop ? "Yes" : "No";
-    }
+        public Scrollbar progressBar;
+        public TextMeshProUGUI cycleText;
+        public TextMeshProUGUI stoppingCriteriaEnbaledText;
+        private int _totalCycles;
+        private int _currentCycles = 0;
 
-    void Start()
-    {
-        UpdateProgressBar();
-    }
-
-    public void CompleteCycle()
-    {
-        if (_currentCycles < _totalCycles)
+        public StoppingCriteria(int cycles, bool idleStop)
         {
-            _currentCycles++;
+            _totalCycles = cycles;
+            stoppingCriteriaEnbaledText.text = idleStop ? "Yes" : "No";
+        }
+
+        void Start()
+        {
             UpdateProgressBar();
         }
-    }
 
-    private void UpdateProgressBar()
-    {
-        progressBar.size = (float)_currentCycles / (float)_totalCycles;
-        cycleText.text = $"{_currentCycles}/{_totalCycles}";
+        public void CompleteCycle()
+        {
+            if (_currentCycles < _totalCycles)
+            {
+                _currentCycles++;
+                UpdateProgressBar();
+            }
+        }
+
+        private void UpdateProgressBar()
+        {
+            progressBar.size = (float)_currentCycles / (float)_totalCycles;
+            cycleText.text = $"{_currentCycles}/{_totalCycles}";
+        }
     }
 }
