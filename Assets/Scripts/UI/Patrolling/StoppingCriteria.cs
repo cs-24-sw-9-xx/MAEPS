07731860d28899b5827fb58b4c8d9764e5a4de07
Christian Ziegler Sejersen <72788875+S3JER@users.noreply.github.com>
2024-10-29 13:18:21 +0100

    Added progressbar script (#17)
    * Added progressbar script
    
    * Formated file + Namespace

---
+++ Assets/Scripts/UI/Patrolling/StoppingCriteria.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace MAEPS.UI
+{
+    public class StoppingCriteria : MonoBehaviour
+    {
+        public Scrollbar progressBar;
+        public TextMeshProUGUI cycleText;
+        public TextMeshProUGUI stoppingCriteriaEnbaledText;
+        private int _totalCycles;
+        private int _currentCycles = 0;
+
+        public StoppingCriteria(int cycles, bool idleStop)
+        {
+            _totalCycles = cycles;
+            stoppingCriteriaEnbaledText.text = idleStop ? "Yes" : "No";
+        }
+
+        void Start()
+        {
+            UpdateProgressBar();
+        }
+
+        public void CompleteCycle()
+        {
+            if (_currentCycles < _totalCycles)
+            {
+                _currentCycles++;
+                UpdateProgressBar();
+            }
+        }
+
+        private void UpdateProgressBar()
+        {
+            progressBar.size = (float)_currentCycles / (float)_totalCycles;
+            cycleText.text = $"{_currentCycles}/{_totalCycles}";
+        }
+    }
+}
