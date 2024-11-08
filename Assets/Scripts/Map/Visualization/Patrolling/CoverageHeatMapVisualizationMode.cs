4d141356fef7008f903205e234ae8b50d98fc062
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2024-11-08 12:43:47 +0100

    Adds heatmap patrolling and coverage

---
+++ Assets/Scripts/Map/Visualization/Patrolling/CoverageHeatMapVisualizationMode.cs
@@ -0,0 +1,34 @@
+using Maes.Statistics;
+
+using UnityEngine;
+
+namespace Maes.Map.Visualization.Patrolling
+{
+    public class PatrollingCoverageHeatMapVisualizationMode : IPatrollingVisualizationMode
+    {
+        private SimulationMap<PatrollingCell> _map;
+        private readonly int _logicTicksBeforeCold = GlobalSettings.TicksBeforeWaypointCoverageHeatMapCold;
+
+        public PatrollingCoverageHeatMapVisualizationMode(SimulationMap<PatrollingCell> map)
+        {
+            _map = map;
+        }
+
+        public void UpdateVisualization(PatrollingVisualizer visualizer, int currentTick)
+        {
+            visualizer.SetAllColors(_map, (cell) => CellToColor(cell, currentTick));
+        }
+
+        private Color32 CellToColor(PatrollingCell cell, int currentTick)
+        {
+            if (!cell.CanBeCovered) return ExplorationVisualizer.SolidColor;
+            if (!cell.IsCovered) return ExplorationVisualizer.StandardCellColor;
+            
+            var ticksSinceLastCovered = currentTick - cell.LastCoverageTimeInTicks;
+            float coldness = Mathf.Min((float) ticksSinceLastCovered / (float) _logicTicksBeforeCold, 1.0f);
+            return Color32.Lerp(ExplorationVisualizer.WarmColor, ExplorationVisualizer.ColdColor, coldness);
+        }
+    }
+    
+    
+}
