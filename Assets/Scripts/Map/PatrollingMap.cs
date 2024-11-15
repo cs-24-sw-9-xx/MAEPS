d0809b6a7a04c5236c91ccf44db21d3d49120dea
S3JER <72788875+S3JER@users.noreply.github.com>
2024-11-15 12:26:50 +0100

    Centerpoint generation for triangles
    Missing adding neighboring points togther

--- Assets/Scripts/Map/PatrollingMap.cs
+++ Assets/Scripts/Map/PatrollingMap.cs
@@ -9,6 +9,9 @@ namespace Maes.Map
     {
         public readonly IReadOnlyList<Vertex> Vertices;
 
+        //TODO: Add a better way to debug point generation, its is currently outcommented
+        // public IReadOnlyList<Vertex> DebugPoints { get; set; }
+
         public PatrollingMap(IEnumerable<Vertex> vertices)
         {
             Vertices = vertices.ToList();
