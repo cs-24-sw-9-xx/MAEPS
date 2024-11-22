d82a689fbadc01afd7cbb13f640c58e1d1be5d96
Mads Mogensen <mail@madsmogensen.dk>
2024-11-22 12:40:15 +0100

    Optimize visualizers
    Only run visualizers once per frame.
    Cache delegates.
    Cache components.
    
    Remove unused packages.

--- Assets/Scripts/Trackers/ITracker.cs
+++ Assets/Scripts/Trackers/ITracker.cs
@@ -4,6 +4,8 @@ namespace Maes.Trackers
 {
     public interface ITracker
     {
+        void UIUpdate();
+
         void LogicUpdate(MonaRobot[] robots);
 
         void SetVisualizedRobot(MonaRobot? robot);
