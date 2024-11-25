a7d4346fde20d098d04f328417435b89ec5090ca
Casper-NS <caspernyvang@gmail.com>
2024-11-25 13:17:10 +0100

    use different path when colliding

--- Assets/Scripts/PatrollingAlgorithms/PatrollingAlgorithm.cs
+++ Assets/Scripts/PatrollingAlgorithms/PatrollingAlgorithm.cs
@@ -80,7 +80,15 @@ namespace Maes.PatrollingAlgorithms
 
             if (_currentPath.Count != 0)
             {
-                PathAndMoveToTarget();
+                if (_controller.IsCurrentlyColliding)
+                {
+                    _controller.PathAndMoveTo(TargetVertex.Position);
+
+                }
+                else
+                {
+                    PathAndMoveToTarget();
+                }
                 return;
             }
 
