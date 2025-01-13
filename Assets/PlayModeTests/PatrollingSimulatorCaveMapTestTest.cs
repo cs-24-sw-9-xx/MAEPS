6a803c1138142fbc34c6e15476f6ece5ecb8e058
Henrik van Peet <72788863+henneboy@users.noreply.github.com>
2025-01-13 12:51:12 +0100

    Add tests that run patrolling until completion (#159)
    * Add tests that run patrolling until completion
    
    Both on building map and cave map
    
    * Formatting
    
    * Remove private method
    
    * don't use broken collision map
    
    ---------
    
    Co-authored-by: Mads Mogensen <mail@madsmogensen.dk>

---
+++ Assets/PlayModeTests/PatrollingSimulatorCaveMapTestTest.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using Maes.Map.MapGen;
+using Maes.PatrollingAlgorithms;
+using Maes.Simulation;
+using Maes.Simulation.SimulationScenarios;
+
+using NUnit.Framework;
+
+using UnityEngine;
+
+namespace PlayModeTests
+{
+    using MySimulationScenario = PatrollingSimulationScenario;
+    using MySimulator = PatrollingSimulator;
+
+    public class PatrollingSimulatorCaveMapTestTest : MonoBehaviour
+    {
+        private MySimulator _maes;
+        private PatrollingSimulation _simulation;
+
+        private void InitializeTestingSimulator()
+        {
+            _maes = new MySimulator();
+            var random = new System.Random(12345);
+            var mapSize = 50;
+
+            var constraintName = "Global";
+            var robotConstraints = StandardTestingConfiguration.GlobalRobotConstraints();
+            var mapConfig = new CaveMapConfig(123, widthInTiles: mapSize, heightInTiles: mapSize, brokenCollisionMap: false);
+            var algoName = "conscientious_reactive";
+            const int robotCount = 1;
+            var spawningPosList = new List<Vector2Int>();
+            for (var amountOfSpawns = 0; amountOfSpawns < robotCount; amountOfSpawns++)
+            {
+                spawningPosList.Add(new Vector2Int(random.Next(0, mapSize), random.Next(0, mapSize)));
+            }
+
+            _maes.EnqueueScenario(
+                new MySimulationScenario(
+                    seed: 123,
+                    totalCycles: 4,
+                    stopAfterDiff: false,
+                    mapSpawner: generator => generator.GenerateMap(mapConfig),
+                    robotSpawner: (buildingConfig, spawner) => spawner.SpawnRobotsAtPositions(
+                        collisionMap: buildingConfig,
+                        seed: 123,
+                        numberOfRobots: robotCount,
+                        spawnPositions: spawningPosList,
+                        createAlgorithmDelegate: (_) => new ConscientiousReactiveAlgorithm()),
+                    statisticsFileName: $"{algoName}-seed-{mapConfig.RandomSeed}-size-{mapSize}-comms-{constraintName}-robots-{robotCount}-SpawnTogether",
+                    robotConstraints: robotConstraints
+                )
+            );
+            _simulation = _maes.SimulationManager.CurrentSimulation;
+        }
+
+        [TearDown]
+        public void ClearSimulator()
+        {
+            _maes.Destroy();
+        }
+
+        [Test(ExpectedResult = null)]
+        public IEnumerator TestConscientiousReactiveCaveMap()
+        {
+            InitializeTestingSimulator();
+            _maes.SimulationManager.AttemptSetPlayState(Maes.UI.SimulationPlayState.FastAsPossible);
+            while (!_simulation.HasFinishedSim())
+            {
+                yield return null;
+            }
+            Assert.True(_simulation.HasFinishedSim());
+        }
+    }
+}
