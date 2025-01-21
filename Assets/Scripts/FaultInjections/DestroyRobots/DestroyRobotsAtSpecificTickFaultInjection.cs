141caf0a0c036ccf7c997f2f1df46e8769710880
Mads Mogensen <mail@madsmogensen.dk>
2025-01-21 12:54:26 +0100

    Cleanup fault injection

--- Assets/Scripts/FaultInjections/DestroyRobots/DestroyRobotsAtSpecificTickFaultInjection.cs
+++ Assets/Scripts/FaultInjections/DestroyRobots/DestroyRobotsAtSpecificTickFaultInjection.cs
@@ -8,24 +8,22 @@ namespace Maes.FaultInjections.DestroyRobots
     public class DestroyRobotsAtSpecificTickFaultInjection : DestroyRobotBaseFaultInjection
     {
         private readonly int[] _destroyAtTicks;
-        private readonly int _currentNumberOfDestroys;
         private int _index;
 
         /// <summary>
         /// Constructor for DestroyRobotsAtSpecificTickFaultInjection.
         /// </summary>
         /// <param name="seed">Seed for the random number generator.</param>
-        /// <param name="destroyAtTicks">Array of ticks at whihch robots should be destroyed.</param>
-        public DestroyRobotsAtSpecificTickFaultInjection(int seed, int[] destroyAtTicks) : base(seed)
+        /// <param name="destroyAtTicks">Ticks at which robots should be destroyed.</param>
+        public DestroyRobotsAtSpecificTickFaultInjection(int seed, params int[] destroyAtTicks) : base(seed)
         {
             _destroyAtTicks = destroyAtTicks;
             Array.Sort(_destroyAtTicks);
-            _currentNumberOfDestroys = destroyAtTicks.Length;
         }
 
         protected override bool ShouldDestroy(int logicTick)
         {
-            if (_index < _currentNumberOfDestroys && logicTick == _destroyAtTicks[_index])
+            if (_index < _destroyAtTicks.Length && logicTick == _destroyAtTicks[_index])
             {
                 _index++;
                 return true;
