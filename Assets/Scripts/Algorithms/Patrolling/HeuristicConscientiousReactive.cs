5482cad403264affdf91e79db4aec212ab087623
Henrik van Peet <henrik_peet@hotmail.com>
2025-02-19 13:03:24 +0100

    Implement a scuffed version

--- Assets/Scripts/Algorithms/Patrolling/HeuristicConscientiousReactive.cs
+++ Assets/Scripts/Algorithms/Patrolling/HeuristicConscientiousReactive.cs
@@ -7,6 +7,7 @@ namespace Maes.Algorithms.Patrolling
     /// <summary>
     /// Original implementation of the Heuristic Conscientious Reactive Algorithm of https://repositorio.ufpe.br/handle/123456789/2474.
     /// Pseudocode can be found in another paper: https://doi.org/10.1080/01691864.2013.763722
+    // Heuristic: The vertex with the lowest idleness and distance estimation
     /// </summary>
     public class HeuristicConscientiousReactiveAlgorithm : PatrollingAlgorithm
     {
@@ -14,14 +15,26 @@ namespace Maes.Algorithms.Patrolling
 
         protected override Vertex NextVertex(Vertex currentVertex)
         {
-            var idleness = currentVertex.Neighbors.Select(x => x.LastTimeVisitedTick).ToArray();
-            var minIdleness = idleness.Min();
-            var maxIdleness = idleness.Max();
-            var normalizedIdleness = idleness.Select(x => (x - minIdleness) / (maxIdleness - minIdleness)).ToArray();
+            // Calculate the normalized idleness of the neighbors
+            var idleness = currentVertex.Neighbors.Select(x => (x.Id, x.LastTimeVisitedTick));
+            var minIdleness = idleness.Min(x => x.LastTimeVisitedTick);
+            var maxIdleness = idleness.Max(x => x.LastTimeVisitedTick);
+            var normalizedIdleness = idleness.Select(x => (id: x.Id, normalizedIdleness: (x.LastTimeVisitedTick - minIdleness) / (maxIdleness - minIdleness)));
 
-            var distanceEstimation = currentVertex.Neighbors.Select(x => _controller.EstimateDistanceToTarget(x.Position)).ToArray();
-            var maxDistanceEstimation = distanceEstimation.Max();
-            return currentVertex.Neighbors.OrderBy(x => x.LastTimeVisitedTick).First();
+            // Calculate the normalized distance estimation of the neighbors
+            var distanceEstimation = currentVertex.Neighbors.Select(x => (id: x.Id, dist: _controller.EstimateDistanceToTarget(x.Position)));
+            var maxDistanceEstimation = distanceEstimation.Max(x => x.dist);
+            distanceEstimation = distanceEstimation.Select(x => (id: x.id, dist: x.dist / maxDistanceEstimation));
+
+            if (normalizedIdleness.Count() != distanceEstimation.Count())
+            {
+                throw new System.Exception("Length of normalizedIdleness and distanceEstimation must be equal");
+            }
+            var result = from idlenessValue in normalizedIdleness
+                         join distanceValue in distanceEstimation on idlenessValue.id equals distanceValue.id
+                         orderby idlenessValue.normalizedIdleness + distanceValue.dist ascending
+                         select (id: idlenessValue.id, value: idlenessValue.normalizedIdleness + distanceValue.dist);
+            return currentVertex.Neighbors.Single(x => x.Id == result.First().id);
         }
     }
 }
