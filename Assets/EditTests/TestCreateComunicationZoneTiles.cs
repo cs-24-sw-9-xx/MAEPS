9590dd429e09e57714bd720d4aa06535f365114b
S3JER <72788875+S3JER@users.noreply.github.com>
2025-03-05 14:12:57 +0100

    Reorder usings

--- Assets/EditTests/TestCreateComunicationZoneTiles.cs
+++ Assets/EditTests/TestCreateComunicationZoneTiles.cs
@@ -1,12 +1,37 @@
+// Copyright 2025 MAEPS
+// 
+// This file is part of MAEPS
+// 
+// MAEPS is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+// 
+// MAEPS is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with MAEPS. If not, see http://www.gnu.org/licenses/.
+//
+// Contributors 2025: 
+// Casper Nyvang Sørensen,
+// Christian Ziegler Sejersen,
+// Jakob Meyer Olsen
+using System;
+using System.Collections.Generic;
+
+using Maes;
 using Maes.Map;
 using Maes.Map.MapGen;
-using UnityEngine;
 using Maes.Robot;
-using Maes;
-using System.Collections.Generic;
-using System;
 using Maes.Utilities;
+
 using NUnit.Framework;
+
+using UnityEngine;
+
 namespace EditTests
 {
     public class CommunicationZoneVerticesTests
