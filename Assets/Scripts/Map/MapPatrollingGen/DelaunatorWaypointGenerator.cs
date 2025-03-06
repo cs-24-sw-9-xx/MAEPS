77e7f43a4f554e4d7c63748688bc7895d7432166
Henrik van Peet <henrik_peet@hotmail.com>
2025-03-06 09:55:44 +0100

    Refactor delaunator to use bitmap

--- Assets/Scripts/Map/MapPatrollingGen/DelaunatorWaypointGenerator.cs
+++ Assets/Scripts/Map/MapPatrollingGen/DelaunatorWaypointGenerator.cs
@@ -23,11 +23,11 @@
 // Mads Beyer Mogensen,
 // Puvikaran Santhirasegaram
 using System.Collections.Generic;
-using System.Linq;
 
 using DelaunatorSharp;
 
 using Maes.Map.MapGen;
+using Maes.Utilities;
 
 using UnityEngine;
 
@@ -44,19 +44,21 @@ namespace Maes.Map.MapPatrollingGen
         public static List<Vertex> GetPossibleWaypoints(SimulationMap<Tile> simulationMap)
         {
             // Get all wall tiles
-            var wallTiles = GetWallsTiles(simulationMap);
+            var wallTiles = GetWallTiles(simulationMap);
 
             // The delaunator library requires a list of IPoint, so we convert the Vector2Int to IPoint
             var points = new List<IPoint>();
 
             // The centerCoordinatePoints will be used to generate the centerpoints of the delaunator triangles
             var centerCoordinatePoints = new List<Vertex>();
-
-            foreach (var wallTile in wallTiles)
+            for (var x = 0; x < simulationMap.WidthInTiles; x++)
             {
-                if (IsCornerTile(wallTile, wallTiles))
+                for (var y = 0; y < simulationMap.HeightInTiles; y++)
                 {
-                    points.Add(new Point(wallTile.x, wallTile.y));
+                    if (wallTiles.Contains(x, y) && IsCornerTile(new Vector2Int(x, y), wallTiles))
+                    {
+                        points.Add(new Point(x, y));
+                    }
                 }
             }
 
@@ -71,14 +73,15 @@ namespace Maes.Map.MapPatrollingGen
             //TODO: Connect neighboring centerpoints with edges, currently only the centerpoints are generated
             return centerCoordinatePoints;
         }
-        private static List<Vector2Int> GetWallsTiles(SimulationMap<Tile> simulationMap)
+
+        private static Bitmap GetWallTiles(SimulationMap<Tile> simulationMap)
         {
-            var wallTiles = new List<Vector2Int>();
             var width = simulationMap.WidthInTiles;
             var height = simulationMap.HeightInTiles;
+            var wallTiles = new Bitmap(0, 0, width, height);
 
             // The outer wall is two tiles thick, so we start at 1 and end at width-1 and height-1
-            // we asume that the walls are one tile thick
+            // we assume that the walls are one tile thick
             for (var x = 0; x <= width; x++)
             {
                 for (var y = 0; y <= height; y++)
@@ -87,7 +90,7 @@ namespace Maes.Map.MapPatrollingGen
                     var firstTri = tile.GetTriangles()[0];
                     if (Tile.IsWall(firstTri.Type))
                     {
-                        wallTiles.Add(new Vector2Int(x, y));
+                        wallTiles.Set(x, y);
                     }
                 }
             }
@@ -101,22 +104,22 @@ namespace Maes.Map.MapPatrollingGen
         /// <param name="tile"></param>
         /// <param name="tiles"></param>
         /// <returns></returns>
-        private static bool IsCornerTile(Vector2Int tile, List<Vector2Int> tiles)
+        private static bool IsCornerTile(Vector2Int tile, Bitmap tiles)
         {
             // Horizontal grid check
-            if (tiles.Any(t => t == tile + Vector2Int.right)
-            && tiles.Any(t => t == tile + Vector2Int.left)
-            && tiles.All(t => t != tile + Vector2Int.up)
-            && tiles.All(t => t != tile + Vector2Int.down))
+            if (tiles.Contains(tile + Vector2Int.right)
+            && tiles.Contains(tile + Vector2Int.left)
+            && tiles.Contains(tile + Vector2Int.up)
+            && tiles.Contains(tile + Vector2Int.down))
             {
                 return false;
             }
 
             // Vertical grid check
-            if (tiles.Any(t => t == tile + Vector2Int.up)
-            && tiles.Any(t => t == tile + Vector2Int.down)
-            && tiles.All(t => t != tile + Vector2Int.right)
-            && tiles.All(t => t != tile + Vector2Int.left))
+            if (tiles.Contains(tile + Vector2Int.up)
+            && tiles.Contains(tile + Vector2Int.down)
+            && tiles.Contains(tile + Vector2Int.right)
+            && tiles.Contains(tile + Vector2Int.left))
             {
                 return false;
             }
