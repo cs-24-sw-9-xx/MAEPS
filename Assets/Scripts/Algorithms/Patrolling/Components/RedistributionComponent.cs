2e562bcf02605c52eae6ede584a89c5fd6845b46
S3JER <72788875+S3JER@users.noreply.github.com>
2025-03-25 12:56:04 +0100

    Fixed warnings

--- Assets/Scripts/Algorithms/Patrolling/Components/RedistributionComponent.cs
+++ Assets/Scripts/Algorithms/Patrolling/Components/RedistributionComponent.cs
@@ -1,3 +1,25 @@
+// Copyright 2025 MAEPS
+// 
+// This file is part of MAEPS
+// 
+// MAEPS is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+// 
+// MAEPS is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with MAEPS. If not, see http://www.gnu.org/licenses/.
+//
+// Contributors 2025: 
+// Casper Nyvang Sørensen,
+// Christian Ziegler Sejersen,
+// Jakob Meyer Olsen
+// Mads Beyer Mogensen
 using System.Collections.Generic;
 
 using Maes.Map;
@@ -21,7 +43,6 @@ namespace Maes.Algorithms.Patrolling.Components
         public int PreUpdateOrder => 1;
 
         /// <inheritdoc />
-
         public int PostUpdateOrder => 1;
 
         public Dictionary<int, bool> RecivedCommunication { get; set; }
@@ -33,6 +54,8 @@ namespace Maes.Algorithms.Patrolling.Components
             _currentPartition = _partitions[_monaRobot.AssignedPartition];
             _communicationManager = controller.CommunicationManager;
             _redistributionTracker = new Dictionary<int, float>();
+            _currentPartitionIntersection = new List<int>();
+            RecivedCommunication = new Dictionary<int, bool>();
         }
 
         public IEnumerable<ComponentWaitForCondition> PreUpdateLogic()
@@ -74,6 +97,7 @@ namespace Maes.Algorithms.Patrolling.Components
                         if (RecivedCommunication[partitionId])
                         {
                             _redistributionTracker[partitionId] = -_currentPartition.CommunicationRatio[partitionId];
+                            RecivedCommunication[partitionId] = false;
                         }
                         else
                         {
@@ -87,10 +111,15 @@ namespace Maes.Algorithms.Patrolling.Components
 
         private sealed class RedistributionMessage
         {
-            public MonaRobot Sender { get; set; }
-            public string Message { get; set; }
+            public MonaRobot Sender { get; }
+            public string Message { get; }
             public int Timestamp { get; }
-
+            public RedistributionMessage(int timestamp, MonaRobot sender, string message)
+            {
+                Timestamp = timestamp;
+                Sender = sender;
+                Message = message;
+            }
         }
     }
 }
