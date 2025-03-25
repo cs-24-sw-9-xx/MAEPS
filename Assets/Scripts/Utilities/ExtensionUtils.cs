f3bfb4d635edfe96b51cfab492f3c5f6c7db3e24
S3JER <72788875+S3JER@users.noreply.github.com>
2025-03-25 12:45:43 +0100

    Init of Redistribution component

--- Assets/Scripts/Utilities/ExtensionUtils.cs
+++ Assets/Scripts/Utilities/ExtensionUtils.cs
@@ -94,5 +94,10 @@ namespace Maes.Utilities
 
             return cellIndexTriangleIndexes;
         }
+
+        public static Vector2Int ToVector2Int(this Vector3 vector)
+        {
+            return new Vector2Int((int)vector.x, (int)vector.y);
+        }
     }
 }
