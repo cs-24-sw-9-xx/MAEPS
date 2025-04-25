3ba3d32805638727731b5cac99403d80f97714ee
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2025-04-25 10:47:55 +0200

    Fix test

--- Assets/Tests/PlayModeTests/Algorithms/Patrolling/HMPPatrollingAlgorithmTests/HmpPatrollingOwnPartitionTest.cs
+++ Assets/Tests/PlayModeTests/Algorithms/Patrolling/HMPPatrollingAlgorithmTests/HmpPatrollingOwnPartitionTest.cs
@@ -20,7 +20,6 @@
 // Mads Beyer Mogensen,
 // Puvikaran Santhirasegaram
 
-/*using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -29,6 +28,7 @@ using Maes.Algorithms.Patrolling;
 using Maes.Map;
 using Maes.Map.Generators;
 using Maes.Map.Generators.Patrolling.Partitioning;
+using Maes.Map.Generators.Patrolling.Partitioning.MeetingPoints;
 using Maes.Map.Generators.Patrolling.Waypoints.Generators;
 using Maes.Robot;
 using Maes.Simulation.Patrolling;
@@ -44,16 +44,10 @@ namespace Tests.PlayModeTests.Algorithms.Patrolling.HMPPatrollingAlgorithmTests
         private PatrollingSimulator _maes;
         private const int Seed = 1;
         private const int MaxSimulatedLogicTicks = 250000;
-        private const int MapSize = 100;
-        private const int RobotCount = 3;
+        private const int MapSize = 50;
+        private const int RobotCount = 2;
         private const int TotalCycles = 3;
         private TrackerVertices _trackerVertices;
-
-        [SetUp]
-        public void Setup()
-        {
-            _maes = new PatrollingSimulator();
-        }
 
         [TearDown]
         public void ClearSimulator()
@@ -64,25 +58,24 @@ namespace Tests.PlayModeTests.Algorithms.Patrolling.HMPPatrollingAlgorithmTests
         [Test(ExpectedResult = null)]
         public IEnumerator Test_RobotsPatrolOnlyItsPartition()
         {
+            _trackerVertices = new TrackerVertices();
             CreateAndEnqueueScenario();
 
             var simulation = _maes.SimulationManager.CurrentSimulation!;
-            _trackerVertices = new TrackerVertices(simulation.Robots);
 
             _maes.SimulationManager.AttemptSetPlayState(Maes.UI.SimulationPlayState.FastAsPossible);
-            while (!simulation.HasFinishedSim() && simulation.SimulatedLogicTicks < MaxSimulatedLogicTicks)
+            while (!_trackerVertices.CheckIsRobotPatrollingOwnPartition() && !simulation.HasFinishedSim() && simulation.SimulatedLogicTicks < MaxSimulatedLogicTicks)
             {
                 yield return null;
             }
 
-            foreach (var (robotId, expected) in _trackerVertices.GeneratedRobotVertices)
-            {
-                Assert.True(_trackerVertices.ObservedRobotVertices[robotId].SetEquals(expected), $"Robot {robotId} did not visit all vertices in its partition.");
-            }
+            Assert.IsTrue(_trackerVertices.IsRobotPatrollingOwnPartition, "Robots are not patrolling their own partition or not visited all its vertices.");
         }
 
         private void CreateAndEnqueueScenario()
         {
+            _maes = new PatrollingSimulator();
+
             var robotConstraints = new RobotConstraints(
                 senseNearbyAgentsRange: 5f,
                 senseNearbyAgentsBlockedByWalls: true,
@@ -110,7 +103,7 @@ namespace Tests.PlayModeTests.Algorithms.Patrolling.HMPPatrollingAlgorithmTests
                         seed: Seed,
                         numberOfRobots: RobotCount,
                         suggestedStartingPoint: null,
-                        createAlgorithmDelegate: _ => new WrapperHMPPatrollingAlgorithm(new AdapterToPartitionGenerator(SpectralBisectionPartitioningGenerator.Generator), algorithm => _trackerVertices.AddGeneratedRobotVertices(algorithm))),
+                        createAlgorithmDelegate: _ => new WrapperHMPPatrollingAlgorithm(_trackerVertices)),
                     mapSpawner: generator => generator.GenerateMap(mapConfig),
                     robotConstraints: robotConstraints,
                     statisticsFileName: $"test",
@@ -120,31 +113,16 @@ namespace Tests.PlayModeTests.Algorithms.Patrolling.HMPPatrollingAlgorithmTests
 
         private class TrackerVertices
         {
-            public TrackerVertices(List<MonaRobot> robots)
+            private readonly Dictionary<int, HashSet<int>> _observedRobotVertices = new();
+            private readonly Dictionary<int, HashSet<int>> _generatedRobotVertices = new();
+
+            public void AddRobot(int robotId, HMPPatrollingAlgorithm algorithm)
             {
-                foreach (var robot in robots)
+                _observedRobotVertices[robotId] = new HashSet<int>();
+                algorithm.SubscribeOnReachVertex(vertexId =>
                 {
-                    ObservedRobotVertices[robot.id] = new HashSet<int>();
-                    if (robot.Algorithm is IPatrollingAlgorithm algorithm)
-                    {
-                        algorithm.SubscribeOnReachVertex(vertexId =>
-                        {
-                            OnReachVertex(robot.id, vertexId);
-                        });
-                    }
-                    else
-                    {
-                        Debug.LogError($"Robot {robot.id} does not have a IPatrollingAlgorithm.");
-                    }
-                }
-            }
-
-            public readonly Dictionary<int, HashSet<int>> ObservedRobotVertices = new();
-            public readonly Dictionary<int, HashSet<int>> GeneratedRobotVertices = new();
-
-            private void OnReachVertex(int robotId, int vertexId)
-            {
-                ObservedRobotVertices[robotId].Add(vertexId);
+                    _observedRobotVertices[robotId].Add(vertexId);
+                });
             }
 
             public void AddGeneratedRobotVertices(HMPPatrollingAlgorithm algorithm)
@@ -152,21 +130,45 @@ namespace Tests.PlayModeTests.Algorithms.Patrolling.HMPPatrollingAlgorithmTests
                 var partitionInfo = algorithm.PartitionInfo;
                 if (partitionInfo is { VertexIds: { Count: > 0 } })
                 {
-                    GeneratedRobotVertices[partitionInfo.RobotId] = new HashSet<int>(partitionInfo.VertexIds);
+                    _generatedRobotVertices[partitionInfo.RobotId] = new HashSet<int>(partitionInfo.VertexIds);
                 }
+            }
+
+            public bool IsRobotPatrollingOwnPartition { get; private set; }
+
+            public bool CheckIsRobotPatrollingOwnPartition()
+            {
+                if (_generatedRobotVertices.Count == 0)
+                {
+                    IsRobotPatrollingOwnPartition = false;
+                    return IsRobotPatrollingOwnPartition;
+                }
+
+                foreach (var (robotId, expected) in _generatedRobotVertices)
+                {
+                    if (!_observedRobotVertices[robotId].SetEquals(expected))
+                    {
+                        IsRobotPatrollingOwnPartition = false;
+                        return IsRobotPatrollingOwnPartition;
+                    }
+                }
+
+                IsRobotPatrollingOwnPartition = true;
+                return IsRobotPatrollingOwnPartition;
             }
         }
 
         private class WrapperHMPPatrollingAlgorithm : IPatrollingAlgorithm
         {
-            public WrapperHMPPatrollingAlgorithm(IPartitionGenerator partitionGenerator, Action<HMPPatrollingAlgorithm> onLogicUpdate)
+            public WrapperHMPPatrollingAlgorithm(TrackerVertices trackerVertices)
             {
-                _algorithm = new HMPPatrollingAlgorithm(partitionGenerator);
-                _onLogicUpdate = onLogicUpdate;
+                _algorithm = new HMPPatrollingAlgorithm(new MeetingPointTimePartitionGenerator(
+                    new AdapterToPartitionGenerator(SpectralBisectionPartitioningGenerator.Generator)));
+                _trackerVertices = trackerVertices;
             }
 
             private readonly HMPPatrollingAlgorithm _algorithm;
-            private readonly Action<HMPPatrollingAlgorithm> _onLogicUpdate;
+            private readonly TrackerVertices _trackerVertices;
 
             public string AlgorithmName => _algorithm.AlgorithmName;
             public Vertex TargetVertex => _algorithm.TargetVertex;
@@ -190,20 +192,20 @@ namespace Tests.PlayModeTests.Algorithms.Patrolling.HMPPatrollingAlgorithmTests
             {
                 foreach (var result in _algorithm.PreUpdateLogic())
                 {
-                    _onLogicUpdate(_algorithm);
+                    _trackerVertices.AddGeneratedRobotVertices(_algorithm);
                     yield return result;
                 }
             }
 
             public IEnumerable<WaitForCondition> UpdateLogic()
             {
-
                 return _algorithm.UpdateLogic();
             }
 
             public void SetController(Robot2DController controller)
             {
                 _algorithm.SetController(controller);
+                _trackerVertices.AddRobot(controller.Id, _algorithm);
             }
 
             public string GetDebugInfo()
@@ -212,4 +214,4 @@ namespace Tests.PlayModeTests.Algorithms.Patrolling.HMPPatrollingAlgorithmTests
             }
         }
     }
-}*/
+}
