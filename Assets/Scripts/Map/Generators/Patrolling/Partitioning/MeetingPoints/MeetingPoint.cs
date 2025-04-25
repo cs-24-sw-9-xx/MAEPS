b9c9d814116b06b12dd53098a8034214bd979aca
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2025-04-25 13:26:01 +0200

    Make the MeetingPoint more immutable

--- Assets/Scripts/Map/Generators/Patrolling/Partitioning/MeetingPoints/MeetingPoint.cs
+++ Assets/Scripts/Map/Generators/Patrolling/Partitioning/MeetingPoints/MeetingPoint.cs
@@ -4,14 +4,15 @@ namespace Maes.Map.Generators.Patrolling.Partitioning.MeetingPoints
 {
     public class MeetingPoint
     {
-        public MeetingPoint(int vertexId, int atTicks)
+        public MeetingPoint(int vertexId, int atTicks, IReadOnlyCollection<int> robotIds)
         {
             VertexId = vertexId;
             AtTicks = atTicks;
+            RobotIds = robotIds;
         }
 
         public int VertexId { get; }
-        public int AtTicks { get; set; }
-        public HashSet<int> RobotIds { get; } = new();
+        public int AtTicks { get; }
+        public IReadOnlyCollection<int> RobotIds { get; }
     }
 }
