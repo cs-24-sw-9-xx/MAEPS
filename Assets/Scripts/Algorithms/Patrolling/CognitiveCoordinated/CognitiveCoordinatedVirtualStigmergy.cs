17e5061c769e7669514c82bb53dae355b42d8d0f
Henrik van Peet <henrik_peet@hotmail.com>
2025-04-28 13:01:15 +0200

    Created cc-virtual-stigmergy

---
+++ Assets/Scripts/Algorithms/Patrolling/CognitiveCoordinated/CognitiveCoordinatedVirtualStigmergy.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Maes.Map;
+
+namespace Maes.Algorithms.Patrolling
+{
+    /// <summary>
+    /// This is a distributed version of the Cognitive Coordinated Algorithm.
+    /// It uses virtual stigmergy to coordinate the robots.
+    /// Each robot has its own local map and uses the occupied vertices of other robots to coordinate.
+    /// </summary>
+    public sealed class CognitiveCoordinatedVirtualStigmergy : CognitiveCoordinatedBase
+    {
+        public override string AlgorithmName => "Cognitive Coordinated (virtual-stigmergy knowledge) Algorithm";
+        protected override PatrollingMap _globalMap => _patrollingMap;
+
+        public override void InitializeCoordinator(PatrollingMap _)
+        {
+            //_virtualStigmergyComponent.Put(_vertualStigmergyMessageId, new Dictionary<int, Vertex>());
+        }
+
+        public override void OccupyVertex(int robotId, Vertex vertex)
+        {
+#if DEBUG
+            if (!_globalMap.Vertices.Contains(vertex))
+            {
+                throw new ArgumentException($"Vertex ({vertex}) is not a part of GlobalMap.Vertices.", nameof(vertex));
+            }
+#endif
+            var _localKnowledge = _virtualStigmergyComponent.GetNonSending(VirtualStigmergyMessageId)!;
+            _localKnowledge[robotId] = vertex;
+            _virtualStigmergyComponent.Put(VirtualStigmergyMessageId, _localKnowledge);
+        }
+
+        public override IEnumerable<Vertex> GetOccupiedVertices(int robotId)
+        {
+            return _virtualStigmergyComponent.Get(VirtualStigmergyMessageId)
+                    .Where(p => p.Key != robotId)
+                    .Select(p => p.Value);
+        }
+
+        public override IEnumerable<Vertex> GetUnoccupiedVertices(int robotId)
+        {
+            var occupiedVertices = GetOccupiedVertices(robotId);
+            return _globalMap.Vertices.Except(occupiedVertices);
+        }
+    }
+}
