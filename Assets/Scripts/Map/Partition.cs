30ac1f5f7dcc434d7e0863d962574988231ef1da
Casper-NS <caspernyvang@gmail.com>
2025-05-05 10:24:43 +0200

    fixed test

--- Assets/Scripts/Map/Partition.cs
+++ Assets/Scripts/Map/Partition.cs
@@ -53,6 +53,11 @@ namespace Maes.Map
             PartitionId = partitionId;
             Vertices = vertices;
             WaypointsCommunicationZones = communicationZones;
+            if (vertices.Count == 0)
+            {
+                return;
+            }
+
             CommunicationZone = CreatePartitionCommunicationZone(vertices, communicationZones);
             //TODO: Look into the way this bitmap is used, it may cause problems with intersection calculations in the future.
             _bitmapWidth = CommunicationZone.Width;
