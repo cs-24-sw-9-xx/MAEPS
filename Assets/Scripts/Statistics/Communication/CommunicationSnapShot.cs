6e66cea3f9f6071cac6e91e46ba24086b4536ad1
Mads Mogensen <mail@madsmogensen.dk>
2025-05-14 14:00:26 +0200

    Track messages sent and received

--- Assets/Scripts/Statistics/Communication/CommunicationSnapShot.cs
+++ Assets/Scripts/Statistics/Communication/CommunicationSnapShot.cs
@@ -11,14 +11,23 @@ namespace Maes.Statistics.Communication
 
         [TypeConverter(typeof(BooleanNullableToBinaryConverter))]
         public bool? AgentsInterconnected { get; set; }
+
         [TypeConverter(typeof(NullableFloatToStringConverter))]
         public float? BiggestClusterPercentage { get; set; }
 
-        protected CommunicationSnapShot(int tick, bool? agentsInterconnected = null, float? biggestClusterPercentage = null)
+        [TypeConverter(typeof(NullableIntToStringConverter))]
+        public int? ReceivedMessageCount { get; set; }
+
+        [TypeConverter(typeof(NullableIntToStringConverter))]
+        public int? SentMessageCount { get; set; }
+
+        protected CommunicationSnapShot(int tick, bool? agentsInterconnected = null, float? biggestClusterPercentage = null, int? receivedMessageCount = null, int? sentMessageCount = null)
         {
             Tick = tick;
             AgentsInterconnected = agentsInterconnected;
             BiggestClusterPercentage = biggestClusterPercentage;
+            ReceivedMessageCount = receivedMessageCount;
+            SentMessageCount = sentMessageCount;
         }
     }
 }
