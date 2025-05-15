ccfc689dea05501ade3f9467ca4c358c4213c83e
Henrik van Peet <henrik_peet@hotmail.com>
2025-05-15 15:05:56 +0200

    Use the robot count

--- Assets/Scripts/Experiments/Patrolling/GroupA/GroupAParameters.cs
+++ Assets/Scripts/Experiments/Patrolling/GroupA/GroupAParameters.cs
@@ -46,8 +46,8 @@ namespace Maes.Experiments.Patrolling
             { nameof(ConscientiousReactiveAlgorithm), (_) => (null, (_) => new ConscientiousReactiveAlgorithm()) },
 
             // ConscientiousReactiveAlgorithm with partitioning
-            { nameof(ConscientiousReactiveAlgorithm)+ "+partitioning", (_) => ((map) =>
-                PartitioningGenerator.MakePatrollingMapWithSpectralBisectionPartitions(map, 1, CreateRobotConstraints()),
+            { nameof(ConscientiousReactiveAlgorithm)+ "+partitioning", (robotCount) => ((map) =>
+                PartitioningGenerator.MakePatrollingMapWithSpectralBisectionPartitions(map, robotCount, CreateRobotConstraints()),
                  (_) => new ConscientiousReactiveAlgorithm()) },
     
             // The map is different for each seed, so the algorithm can just use the same seed for all maps.
