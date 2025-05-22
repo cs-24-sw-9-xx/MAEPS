814856fec19b7ca89d8da7344c5b005b1342a0ff
Henrik van Peet <henrik_peet@hotmail.com>
2025-05-22 15:56:04 +0200

    HMPPartition variant for immediate takeover
    Only take over if the other robot does not show

---
+++ Assets/Scripts/Algorithms/Patrolling/HMPPatrollingAlgorithms/ImmediateTakeover/MeetingPoints/PossibleMeetingPoint.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+using Maes.Map;
+
+namespace Maes.Assets.Scripts.Algorithms.Patrolling.HMPPatrollingAlgorithms.ImmediateTakeover.MeetingPoints
+{
+    public struct PossibleMeetingPoint
+    {
+        public readonly int Robot1Id;
+        public readonly int Robot2Id;
+        public readonly List<(Vertex, Vertex)> Connections;
+
+        public PossibleMeetingPoint(int robot1Id, int robot2Id)
+        {
+            Robot1Id = robot1Id;
+            Robot2Id = robot2Id;
+            Connections = new List<(Vertex, Vertex)>();
+        }
+    }
+}
