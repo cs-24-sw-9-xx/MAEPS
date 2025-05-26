4720b4ba7230dfe6bc0aeaa5a1910927843dbe1e
S3JER <72788875+S3JER@users.noreply.github.com>
2025-05-26 10:58:41 +0200

    Experiment

---
+++ Assets/Scripts/Experiments/Patrolling/GroupB/AllAlgoTenSeedsExperiment.cs
@@ -0,0 +1,65 @@
+// Copyright 2025 MAEPS
+// 
+// This file is part of MAEPS
+// 
+// MAEPS is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+// 
+// MAEPS is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with MAEPS. If not, see http://www.gnu.org/licenses/.
+//
+// Contributors 2025: 
+// Casper Nyvang Sørensen,
+// Christian Ziegler Sejersen,
+// Jakob Meyer Olsen
+
+using System.Collections.Generic;
+using System.Linq;
+
+using Maes.Simulation.Patrolling;
+using Maes.UI;
+
+using UnityEngine;
+
+namespace Maes.Experiments.Patrolling.GroupB
+{
+    using MySimulator = PatrollingSimulator;
+
+    internal class AllAlgoTenSeedsExperiment : MonoBehaviour
+    {
+        private void Start()
+        {
+            var scenarios = new List<PatrollingSimulationScenario>();
+            foreach (var seed in Enumerable.Range(0, 10))
+            {
+                foreach (var algorithm in GroupBParameters.PartitionedAlgorithms)
+                {
+                    scenarios.AddRange(ScenarioUtil.CreateScenarios(
+                        seed,
+                        algorithm.Key,
+                        algorithm.Value,
+                        16,
+                        200,
+                        GroupBParameters.StandardAmountOfCycles,
+                        GroupBParameters.RobotConstraintsDictionary[algorithm.Key],
+                        4,
+                        GroupBParameters.FaultInjection(seed, robotCount: 16)));
+                }
+            }
+
+            Debug.Log($"Total scenarios scheduled: {scenarios.Count}");
+
+            var simulator = new MySimulator(scenarios);
+
+            simulator.PressPlayButton(); // Instantly enter play mode
+            simulator.SimulationManager.AttemptSetPlayState(SimulationPlayState.FastAsPossible);
+        }
+    }
+}
