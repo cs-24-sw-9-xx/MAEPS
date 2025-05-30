b3bb12e342c68aaae8461b0d6ad2a4d8ea4ab9a6
Henrik van Peet <henrik_peet@hotmail.com>
2025-05-30 14:35:50 +0200

    Map size startup time

---
+++ Assets/Scripts/Experiments/Patrolling/GroupA/GroupAMapSizeStartupTimeExperiment.cs
@@ -0,0 +1,64 @@
+// Copyright 2025 MAEPS
+// 
+// This file is part of MAEPS
+// 
+// MAEPS is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+// 
+// MAEPS is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with MAEPS. If not, see http://www.gnu.org/licenses/.
+// 
+// Contributors: 
+// Henrik van Peet,
+// Mads Beyer Mogensen,
+// Puvikaran Santhirasegaram
+
+using System.Collections.Generic;
+
+using Maes.Algorithms.Patrolling;
+using Maes.Simulation.Patrolling;
+using Maes.UI;
+
+using UnityEngine;
+using UnityEngine.Scripting;
+
+namespace Maes.Experiments.Patrolling
+{
+    using MySimulationScenario = PatrollingSimulationScenario;
+    using MySimulator = PatrollingSimulator;
+
+    /// <summary>
+    /// AAU group cs-25-ds-10-17
+    /// </summary>
+    [Preserve]
+    internal class GroupAMapSizeStartupTimeExperiment : MonoBehaviour
+    {
+        private static readonly List<int> _mapSizes = new() { 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000 };
+
+        private void Start()
+        {
+            var scenarios = new List<MySimulationScenario>();
+            foreach (var seed in GroupAParameters.SeedGenerator(1))
+            {
+                foreach (var mapSize in _mapSizes)
+                {
+                    scenarios.AddRange(GroupAExperimentHelpers.CreateScenarios(seed, "cr", (_) => new ConscientiousReactiveAlgorithm(), null, 4, mapSize, amountOfCycles: 0));
+                }
+            }
+
+            Debug.Log($"Total scenarios scheduled: {scenarios.Count}");
+
+            var simulator = new MySimulator(scenarios);
+
+            simulator.PressPlayButton(); // Instantly enter play mode
+            simulator.SimulationManager.AttemptSetPlayState(SimulationPlayState.FastAsPossible);
+        }
+    }
+}
