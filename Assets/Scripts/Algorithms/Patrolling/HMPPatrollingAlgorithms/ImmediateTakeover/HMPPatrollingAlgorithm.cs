acb98dd88fd7693b22f067120ea7aca3206523ad
Henrik van Peet <henrik_peet@hotmail.com>
2025-06-12 16:46:41 +0200

    Fix takeover algorithms

--- Assets/Scripts/Algorithms/Patrolling/HMPPatrollingAlgorithms/ImmediateTakeover/HMPPatrollingAlgorithm.cs
+++ Assets/Scripts/Algorithms/Patrolling/HMPPatrollingAlgorithms/ImmediateTakeover/HMPPatrollingAlgorithm.cs
@@ -291,7 +291,7 @@ namespace Maes.Algorithms.Patrolling.HMPPatrollingAlgorithms.ImmediateTakeover
         private int EstimatePartitionMeetingIntervalTicks(UnfinishedPartitionInfo partition, int numberOdMeetingPoints)
         {
             var maxTravelTime = EstimateMaxTravelTimeForPartition(partition);
-            return 2 * (int)Math.Ceiling((double)partition.VertexIds.Count / numberOdMeetingPoints) * maxTravelTime;
+            return maxTravelTime + (int)Math.Ceiling((double)partition.VertexIds.Count / numberOdMeetingPoints) * maxTravelTime;
         }
 
         private int EstimateMaxTravelTimeForPartition(UnfinishedPartitionInfo partition)
