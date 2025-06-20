860cd485b267be892d03fb0a112a63e1641669f0
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2025-06-20 14:39:42 +0200

    Grouping

---
+++ MAEPS-Data-Processor/GroupbyAlgorithm/Program.cs
@@ -0,0 +1,23 @@
+﻿using MAEPS.Data.Processor.Preprocessors;
+using MAEPS.Data.Processor.Utilities;
+
+var argumentParser = new ArgumentParser();
+argumentParser.ParseArguments(args);
+
+var experimentsFolderPath = argumentParser.GetArgument("--path");
+if (!Path.Exists(experimentsFolderPath))
+{
+    Console.WriteLine("Invalid path provided");
+    return;
+}
+
+var regenerate = argumentParser.GetArgument("--regenerate", bool.TryParse, true);
+
+var dataFolder = DataPreProcessor.CopyDataFolder(experimentsFolderPath, folderName: "Grouped", regenerate: regenerate);
+
+var mapTypeFolders = DataPreProcessor.SplitMapTypesDataFolder(dataFolder, regenerate: regenerate);
+
+foreach (var (_, folder) in mapTypeFolders)
+{
+    GroupingAlgorithm.GroupScenarioByAlgorithmName(folder);
+}
