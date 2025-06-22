17deac2b110e1dd5ec65f0309ee1c5a4db1e04a0
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2025-06-22 23:30:07 +0200

    Update GroupAExperiment2OverallPerformance.cs

--- Assets/Scripts/Experiments/Patrolling/GroupA/GroupAExperiment2OverallPerformance.cs
+++ Assets/Scripts/Experiments/Patrolling/GroupA/GroupAExperiment2OverallPerformance.cs
@@ -52,8 +52,10 @@ namespace Maes.Experiments.Patrolling
             var scenarios = new List<MySimulationScenario>();
             foreach (var seed in GroupAParameters.SeedGenerator(100))
             {
-                var (patrollingMapFactory, algorithm) = GroupAParameters.FaultTolerantHMPVariants["NoSendAllFaultTolerance.NoMeetEarlyFixup.HMPPatrollingAlgorithm"](GroupAParameters.StandardRobotCount);
-                scenarios.AddRange(GroupAExperimentHelpers.CreateScenarios(seed, "HMP.NotSendAll", algorithm, patrollingMapFactory));
+                var (patrollingMapFactory1, algorithm1) = GroupAParameters.FaultTolerantHMPVariants["NoSendAllFaultTolerance.NoMeetEarlyFixup.HMPPatrollingAlgorithm"](GroupAParameters.StandardRobotCount);
+                var (patrollingMapFactory2, algorithm2) = GroupAParameters.FaultTolerantHMPVariants["FaultTolerance.NoMeetEarlyFixup.HMPPatrollingAlgorithm"](GroupAParameters.StandardRobotCount);
+                scenarios.AddRange(GroupAExperimentHelpers.CreateScenarios(seed, "HMP", algorithm1, patrollingMapFactory1));
+                scenarios.AddRange(GroupAExperimentHelpers.CreateScenarios(seed, "HMP.NotSendAll", algorithm2, patrollingMapFactory2));
             }
 
             if (scenarioFilters is not null && scenarioFilters.Any())
