62665eda95131f9e16d6ea06c130fd1c877e8f1f
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2025-06-22 21:10:00 +0200

    not send all

--- Assets/Scripts/Experiments/Patrolling/GroupA/GroupAExperiment2OverallPerformance.cs
+++ Assets/Scripts/Experiments/Patrolling/GroupA/GroupAExperiment2OverallPerformance.cs
@@ -52,11 +52,8 @@ namespace Maes.Experiments.Patrolling
             var scenarios = new List<MySimulationScenario>();
             foreach (var seed in GroupAParameters.SeedGenerator(100))
             {
-                foreach (var (algorithmName, lambda) in GroupAParameters.AllAlgorithms)
-                {
-                    var (patrollingMapFactory, algorithm) = lambda(GroupAParameters.StandardRobotCount);
-                    scenarios.AddRange(GroupAExperimentHelpers.CreateScenarios(seed, algorithmName, algorithm, patrollingMapFactory));
-                }
+                var (patrollingMapFactory, algorithm) = GroupAParameters.FaultTolerantHMPVariants["NoSendAllFaultTolerance.NoMeetEarlyFixup.HMPPatrollingAlgorithm"](GroupAParameters.StandardRobotCount);
+                scenarios.AddRange(GroupAExperimentHelpers.CreateScenarios(seed, "HMP.NotSendAll", algorithm, patrollingMapFactory));
             }
 
             if (scenarioFilters is not null && scenarioFilters.Any())
